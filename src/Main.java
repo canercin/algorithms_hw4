da83f71cd45a2b646254636ff94e4463d2d32a93
JuvenMi <canercincin34@gmail.com>
2023-06-02 16:32:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        BSTOperations bstOperations = new BSTOperations();
+        BSTNode head = null;
+        int[] arr = {10, 5, 15, 3, 7, 12, 18, 2, 4, 6, 8, 11, 13, 17, 19, 1, 9, 14, 16, 20};
+        for (int i :
+                arr) {
+            head = bstOperations.createBST(head, i);
+        }
+        bstOperations.levelOrderWithLevel(head);
+        System.out.println();
+        System.out.println("----");
+        head = bstOperations.createMirror(head);
+        System.out.println("----");
+        bstOperations.levelOrderWithLevel(head);
+        System.out.println("----");
+        head = bstOperations.createMirror2(head);
+        System.out.println("----");
+        bstOperations.levelOrderWithLevel2(head);
+    }
+}
